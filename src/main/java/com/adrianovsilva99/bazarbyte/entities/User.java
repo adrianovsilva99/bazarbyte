cd8f5d3a811b6b88bf4589b23d4cf747c8d0e439
Adriano Vicente <adrianovic.silva@gmail.com>
2023-05-26 18:47:14 -0300

    OrderItem, OrderItemPK, Composition

--- src/main/java/com/adrianovsilva99/bazarbyte/entities/User.java
+++ src/main/java/com/adrianovsilva99/bazarbyte/entities/User.java
@@ -25,8 +25,7 @@ public class User {
 
     public User() {}
 
-    public User(Long id, String name, String email, String phone,
-                LocalDate birthDate, String password) {
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
