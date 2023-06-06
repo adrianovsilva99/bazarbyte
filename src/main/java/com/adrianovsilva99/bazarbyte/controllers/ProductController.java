71ac8e7e7b02b30c51ddb12e42c4815ee5576758
Adriano Vicente <adrianovic.silva@gmail.com>
2023-06-06 14:08:14 -0300

    GET Products paged

--- src/main/java/com/adrianovsilva99/bazarbyte/controllers/ProductController.java
+++ src/main/java/com/adrianovsilva99/bazarbyte/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.adrianovsilva99.bazarbyte.controllers;
 
 import com.adrianovsilva99.bazarbyte.dto.ProductDTO;
-import com.adrianovsilva99.bazarbyte.entities.Product;
-import com.adrianovsilva99.bazarbyte.repositories.ProductRepository;
 import com.adrianovsilva99.bazarbyte.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,5 +24,10 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
